b897feb4f09648d7ab3d1881b9fb6d7266c2785b
Graeme <southworthgraeme@gmail.com>
2023-11-20 17:41:17 -0500

    Added code to communicate with .java files (Must Test Functionality)

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Your Express server file (e.g., server.js)
 const express = require('express');
 const bodyParser = require('body-parser');
+const { exec } = require('child_process');
 const cors = require('cors');
 
 const app = express();
@@ -11,6 +12,19 @@ app.use(cors());
 app.get("/getGPT", (req, res) => {
     // Access the 'keyword' parameter from the query string
     const ingredients = req.query.keyword || []; // Replace with your default value or validation logic
+
+
+    // Execute the Java file with parameters
+    const javaProcess = exec(`java -cp . YourJavaFile ${ingredients}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing Java file: ${error.message}`);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        console.log(`Java process output: ${stdout}`);
+    });
+    
 
     // Set the 'Access-Control-Allow-Origin' header to allow requests from http://localhost:3000
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -23,6 +37,11 @@ app.get("/getGPT", (req, res) => {
     };
 
     res.json(responseData);
+
+    // Handle Java process exit
+    javaProcess.on('exit', (code) => {
+        console.log(`Java process exited with code ${code}`);
+    });
 });
 
 app.listen(port, () => {
